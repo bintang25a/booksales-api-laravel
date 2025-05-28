e1bfeb2885e17b270a3d3c257e8d0be7e22e3c9e
bintang25a <bintangalfizar25@gmail.com>
2025-05-28 09:10:06 +0700

    menambahkan tugas pertemuan 7

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\TransactionController;
+use App\Models\Transaction;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,10 +17,13 @@ Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
 Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
 Route::apiResource('books', BookController::class)->only(['index', 'show']);
 
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:api'])->group(function() {
+    Route::apiResource('/transactions', TransactionController::class)->only(['store', 'show']);
+    Route::post('/transactions/{id}', [TransactionController::class, 'update']);
 
     Route::middleware(['role:admin'])->group(function() {
         Route::apiResource('/genres', GenreController::class)->only(['store', 'destroy']);
@@ -29,6 +34,8 @@ Route::middleware(['auth:api'])->group(function() {
     
         Route::apiResource('/books', BookController::class)->only(['store', 'destroy']);
         Route::post('/books/{id}', [BookController::class, 'update']);
+
+        Route::apiResource('/transactions', TransactionController::class)->only(['index', 'destroy']);
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
